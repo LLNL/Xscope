96db20a0cf2567929d826d60f477a512dd08a134
Anh Tran <anhb.tran1010@mail.com>
2022-08-25 15:02:51 -0600

    Change library to BOtorch for GPU friendly Xscope

--- bo_analysis.py
+++ bo_analysis.py
@@ -1,17 +1,28 @@
 #!//usr/bin/env python3
 import logging
-from bayes_opt import BayesianOptimization
-from bayes_opt import UtilityFunction
 from functools import partial
 from utils import *
 from test_function import *
+import torch
+from botorch.models import SingleTaskGP
+from botorch.utils import standardize
+from botorch.fit import fit_gpytorch_model
+from gpytorch.mlls import ExactMarginalLogLikelihood
+from botorch.optim import optimize_acqf
+from botorch.acquisition.monte_carlo import qExpectedImprovement
+from botorch.sampling.samplers import SobolQMCNormalSampler
 
 # verbose = False
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+dtype = torch.double
 verbose = True
 CUDA_LIB = ''
 bo_iterations = 25  # number of iteration
 logging.basicConfig(filename='Xscope.log', level=logging.INFO)
 logger = logging.getLogger(__name__)
+BATCH_SIZE = 5
+NUM_RESTARTS = 10
+RAW_SAMPLES = 512
 
 # ----- Status variables ------
 found_inf_pos = False
@@ -19,6 +30,13 @@ found_inf_neg = False
 found_under_pos = False
 found_under_neg = False
 # -----------------------------
+def data_initialization(obj_func, batch_shape, sample_dim):
+    initial_X = torch.cuda.DoubleTensor((batch_shape, sample_dim)).uniform_(-1e+307, 1e+307)
+    initial_Y = []
+    for x in initial_X:
+        initial_Y.append(obj_func(x.detach().numpy()))\
+    initial_Y = torch.Tensor(initial_X, dtype=dtype, device=device)
+    return initial_X, initial_Y
 
 def initialize():
     global found_inf_pos, found_inf_neg, found_under_pos, found_under_neg
@@ -43,43 +61,58 @@ def run_optimizer(bounds, func, new_max, exp_name):
     num_fail = 0
     trials_so_far = 0
     trials_to_trigger = -1
+    train_X, train_Y = data_initialization(func, 10, bounds.shape[2])
+    train_Y = standardize(train_Y)
+    best_Y = train_Y.max()
 
     # if are_we_done(func, 0.0, exp_name):
     #   return
 
-    optimizer = BayesianOptimization(f=func, pbounds=bounds, verbose=2, random_state=1)
+    gp = SingleTaskGP(train_X, train_Y)
+    mll = ExactMarginalLogLikelihood(gp.likelihood, gp)
+    fit_gpytorch_model(mll)
     if verbose: print('BO opt...')
-    utility = UtilityFunction(kind="ei", kappa=2.5, xi=0.1e-1)
-    # utility = UtilityFunction(kind="ucb", kappa=10, xi=0.1e-1)
-    # utility = UtilityFunction(kind="poi", kappa=10, xi=1e-1)
+    sampler = SobolQMCNormalSampler(1024)
+    qEI = qExpectedImprovement(gp, best_Y, sampler)
+
     for i in range(bo_iterations):
         print("iteration: ", i)
         trials_so_far += 1
         try:
-            next_point = optimizer.suggest(utility)
-            next_point = list(next_point.values())
-            target = func(next_point)
-            result_logger.save_results(target, exp_name)
-            logger.info("The input {} resulted in the the exception {}".format(input, target))
-            target = validate_output(target, new_max)
-            optimizer.register(params=next_point, target=target)
-            if i % 10 == 0 and i is not 0:
-                if len(optimizer.space.target) == 0:
-                    utility.xi = utility.xi * 2
+            for bound in bounds:
+                candidates, _ = optimize_acqf(
+                    acq_function=qEI,
+                    bounds=bound,
+                    q=BATCH_SIZE,
+                    num_restarts=NUM_RESTARTS,
+                    raw_samples=RAW_SAMPLES,  # used for intialization heuristic
+                    options={"batch_limit": 5, "maxiter": 200},
+                )
+                new_Y = []
+                new_x = candidates.detach()
+                train_X = torch.cat([train_X, new_x])
+                for x in new_x:
+                    new_Y.append(func(x.numpy()))
+                train_Y = torch.cat([train_Y, new_Y])
+                gp = SingleTaskGP(train_X, train_Y)
+                mll = ExactMarginalLogLikelihood(gp.likelihood, gp)
+                fit_gpytorch_model(mll)
+
         except Exception as e:
-            if isinstance(e, ValueError):
-                num_fail += 1
-                optimizer._space._target[-1] /= (10 ** num_fail)
-            if verbose: print("Oops!", e.__class__, "occurred.")
-            if verbose: print(e)
+            break
+            # if isinstance(e, ValueError):
+            #     num_fail += 1
+            #     optimizer._space._target[-1] /= (10 ** num_fail)
+            # if verbose: print("Oops!", e.__class__, "occurred.")
+            # if verbose: print(e)
             # if verbose: logging.exception("Something awful happened!")
         finally:
+            result_logger.save_results(train_Y.max(), exp_name)
             result_logger.update_runs_table(exp_name)
             continue
 
-    if verbose: print(optimizer.max)
-    val = optimizer.max['target']
-    result_logger.save_results(val, exp_name)
+    if verbose: print(train_Y.max)
+    result_logger.save_results(train_X[train_Y.argmax()], exp_name)
 
 def optimize(shared_lib: str, input_type: str, num_inputs: int, splitting: str, new_max: float):
     test_func.lib = shared_lib
--- utils.py
+++ utils.py
@@ -2,7 +2,11 @@ import math
 import numpy
 import os
 
+import torch
+
 max_normal = 1e+307
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+dtype = torch.double
 
 def validate_output(output, new_max):
     if numpy.isposinf(output):
@@ -13,220 +17,232 @@ def validate_output(output, new_max):
         output = new_max
     return output
 
+
 def bounds(split, num_input, input_type="fp"):
-  b = []
-  upper_lim = max_normal
-  lower_lim = -max_normal
-  if input_type == "exp":
-    upper_lim = 307
-    lower_lim = -307
-  if split == "whole":
-    if num_input == 1:
-      b.append({'x0': (lower_lim, upper_lim)})
-    elif num_input == 2:
-      b.append({'x0': (lower_lim, upper_lim), 'x1': (lower_lim, upper_lim)})
-    else:
-      b.append({'x0': (lower_lim, upper_lim), 'x1': (lower_lim, upper_lim), 'x2': (lower_lim, upper_lim)})
-      
-  elif split == "two":
-    if num_input == 1:
-      b.append({'x0': (lower_lim, 0)})
-      b.append({'x0': (0, upper_lim)})
-    elif num_input == 2:
-      b.append({'x0': (lower_lim, 0), 'x1': (lower_lim, 0)})
-      b.append({'x0': (0, upper_lim), 'x1': (0, upper_lim)})
-    else:
-      b.append({'x0': (lower_lim, 0), 'x1': (lower_lim, 0), 'x2': (lower_lim, 0)})
-      b.append({'x0': (0, upper_lim), 'x1': (0, upper_lim), 'x2': (0, upper_lim)})
-      
-  else:
-    limits = [0.0, 1e-307, 1e-100, 1e-10, 1e-1, 1e0, 1e+1, 1e+10, 1e+100, 1e+307]
-    ranges = []
+    b = []
+    upper_lim = max_normal
+    lower_lim = -max_normal
     if input_type == "exp":
-      for i in range(len(limits)-1):
-        x = limits[i]
-        y = limits[i+1]
-        t = (min(x,y), max(x,y))
-        ranges.append(t)
+        upper_lim = 307
+        lower_lim = -307
+    if split == "whole":
+        if num_input == 1:
+            b.append([[lower_lim], [upper_lim]])
+        elif num_input == 2:
+            b.append([[lower_lim, lower_lim], [upper_lim, upper_lim]])
+        else:
+            b.append([[lower_lim, lower_lim, lower_lim], [upper_lim, upper_lim, upper_lim]])
+        b = torch.as_tensor(b, dtype=dtype, device=device)
+
+    elif split == "two":
+        if num_input == 1:
+            b.append([[lower_lim], [0]])
+            b.append([[0], [upper_lim]])
+        elif num_input == 2:
+            b.append([[lower_lim, lower_lim], [0, 0]])
+            b.append([[0, 0],[upper_lim, upper_lim]])
+        else:
+            b.append([[lower_lim, lower_lim, lower_lim], [0, 0, 0]])
+            b.append([[0, 0,0], [upper_lim, upper_lim, upper_lim]])
+        b = torch.as_tensor(b, dtype=dtype, device=device)
+
     else:
-      for i in range(len(limits)-1):
-        x = limits[i]
-        y = limits[i+1]
-        t = (min(x,y), max(x,y))
-        ranges.append(t)
-        x = -limits[i]
-        y = -limits[i+1]
-        t = (min(x,y), max(x,y))
-        ranges.append(t)
-    if num_input == 1:
-      for r1 in ranges:
-        b.append({'x0': r1})
-    elif num_input == 2:
-      for r1 in ranges:
-        for r2 in ranges:
-          b.append({'x0': r1, 'x1': r2})
-    else:
-      for r1 in ranges:
-        for r2 in ranges:
-          b.append({'x0': r1, 'x1': r2, 'x2': r2})
-  return b
+        limits = [0.0, 1e-307, 1e-100, 1e-10, 1e-1, 1e0, 1e+1, 1e+10, 1e+100, 1e+307]
+        ranges = []
+        if input_type == "exp":
+            for i in range(len(limits) - 1):
+                x = limits[i]
+                y = limits[i + 1]
+                t = (min(x, y), max(x, y))
+                ranges.append(t)
+        else:
+            for i in range(len(limits) - 1):
+                x = limits[i]
+                y = limits[i + 1]
+                t = [[min(x, y)], [max(x, y)]]
+                ranges.append(t)
+                x = -limits[i]
+                y = -limits[i + 1]
+                t = [[min(x, y)], [max(x, y)]]
+                ranges.append(t)
+        if num_input == 1:
+            for r1 in ranges:
+                b.append(torch.tensor([r1], dtype=dtype, device=device))
+        elif num_input == 2:
+            for r1 in ranges:
+                for r2 in ranges:
+                    bound = torch.transpose(torch.tensor([r1,r2], dtype=dtype, device=device).squeeze(),0,1)
+                    b.append(bound)
+        else:
+            for r1 in ranges:
+                for r2 in ranges:
+                    bound = torch.transpose(torch.tensor([r1,r2,r2], dtype=dtype, device=device).squeeze(),0,1)
+                    b.append(bound)
+        b = torch.stack(b, dim=0)
+        b = torch.split(b, 5)
+    return b
+
 
 def is_inf_pos(val):
-  if math.isinf(val):
-    return val > 0.0
-  return False
-
-def is_inf_neg(val):
-  if math.isinf(val):
-    return val < 0.0
-  return False
-
-def is_under_pos(val):
-  if numpy.isfinite(val):
-    if val > 0.0 and val < 2.22e-308:
-      return True
-  return False
-
-def is_under_neg(val):
-  if numpy.isfinite(val):
-    if val < 0.0 and val > -2.22e-308:
-      return True
-  return False
-
-class ResultLogger:
-  def __init__(self):
-    self.results = {}
-    self.runs_results = {}
-    self.trials_so_far = 0
-    self.trials_to_trigger = -1
-    self.trials_results = {}
-    self.random_results = {}
-
-  def save_trials_to_trigger(self, exp_name: str):
-    global trials_to_trigger, trials_so_far
-    if trials_to_trigger == -1:
-      trials_to_trigger = trials_so_far
-      self.trials_results[exp_name] = trials_to_trigger
-
-  def save_results(self, val: float, exp_name: str):
-    # Infinity
     if math.isinf(val):
-      if exp_name not in self.results.keys():
-        if val < 0.0:
-          self.results[exp_name] = [1, 0, 0, 0, 0]
-          self.save_trials_to_trigger(exp_name)
-        else:
-          self.results[exp_name] = [0, 1, 0, 0, 0]
-          self.save_trials_to_trigger(exp_name)
-      else:
-        if val < 0.0:
-          self.results[exp_name][0] += 1
-        else:
-          self.results[exp_name][1] += 1
-
-    # Subnormals
-    if numpy.isfinite(val):
-      if val > -2.22e-308 and val < 2.22e-308:
-        if val != 0.0 and val != -0.0:
-          if exp_name not in self.results.keys():
-            if val < 0.0:
-              self.results[exp_name] = [0, 0, 1, 0, 0]
-              self.save_trials_to_trigger(exp_name)
-            else:
-              self.results[exp_name] = [0, 0, 0, 1, 0]
-              self.save_trials_to_trigger(exp_name)
-          else:
-            if val < 0.0:
-              self.results[exp_name][2] += 1
-            else:
-              self.results[exp_name][3] += 1
-
-    if math.isnan(val):
-      if exp_name not in self.results.keys():
-        self.results[exp_name] = [0, 0, 0, 0, 1]
-        self.save_trials_to_trigger(exp_name)
-      else:
-        self.results[exp_name][4] += 1
-
-  def update_runs_table(self, exp_name: str):
-    if exp_name not in self.runs_results.keys():
-      self.runs_results[exp_name] = 0
-    else:
-      self.runs_results[exp_name] += 1
-
-  def are_we_done(self, func, recent_val, exp_name):
-    global found_inf_pos, found_inf_neg, found_under_pos, found_under_neg
-
-    # Finding INF+
-    if 'max_inf' in func.__name__:
-      if found_inf_pos:
-        return True
-      else:
-        if is_inf_pos(recent_val):
-          found_inf_pos = True
-          self.save_results(recent_val, exp_name)
-          return True
-
-    # Finding INF-
-    elif 'min_inf' in func.__name__:
-      if found_inf_neg:
-        return True
-      else:
-        if is_inf_neg(recent_val):
-          found_inf_neg = True
-          self.save_results(recent_val, exp_name)
-          return True
-
-    # Finding Under-
-    elif 'max_under' in func.__name__:
-      if found_under_neg:
-        return True
-      else:
-        if is_under_neg(recent_val):
-          found_under_neg = True
-          self.save_results(recent_val, exp_name)
-          return True
-
-    # Finding Under+
-    elif 'min_under' in func.__name__:
-      if found_under_pos:
-        return True
-      else:
-        if is_under_pos(recent_val):
-          found_under_pos = True
-          self.save_results(recent_val, exp_name)
-          return True
+        return val > 0.0
     return False
 
-  def get_results(self):
-    return self.results
 
-  def get_random_results(self):
-    return self.random_results
+def is_inf_neg(val):
+    if math.isinf(val):
+        return val < 0.0
+    return False
 
-  def print_result(self, shared_lib, number_sampling, range_splitting, logger):
-    function_key = shared_lib + '|' + number_sampling + '|b_' + range_splitting
-    fun_name = os.path.basename(shared_lib)
-    print('-------------- Results --------------')
-    print(fun_name)
-    if len(self.results.keys()) > 0:
-        total_exception = 0
-        for key in self.results.keys():
-            if function_key in key:
-                print('Range:', key.split('|')[0])
-                print('\tINF+:', self.results[key][0])
-                print('\tINF-:', self.results[key][1])
-                print('\tSUB-:', self.results[key][2])
-                print('\tSUB-:', self.results[key][3])
-                print('\tNaN :', self.results[key][4])
-                # print('\tTotal Exception for range {}: {}'.format(key.split('|')[0], sum(results[key]))
-                total_exception += sum(self.results[key])
-        print('\tTotal Exception: ', total_exception)
-        logger.info('\tTotal Exception: {} '.format(total_exception))
-    else:
-        print('\tINF+:', 0)
-        print('\tINF-:', 0)
-        print('\tSUB-:', 0)
-        print('\tSUB-:', 0)
-        print('\tNaN :', 0)
-    print('')
+
+def is_under_pos(val):
+    if numpy.isfinite(val):
+        if val > 0.0 and val < 2.22e-308:
+            return True
+    return False
+
+
+def is_under_neg(val):
+    if numpy.isfinite(val):
+        if val < 0.0 and val > -2.22e-308:
+            return True
+    return False
+
+
+class ResultLogger:
+    def __init__(self):
+        self.results = {}
+        self.runs_results = {}
+        self.trials_so_far = 0
+        self.trials_to_trigger = -1
+        self.trials_results = {}
+        self.random_results = {}
+
+    def save_trials_to_trigger(self, exp_name: str):
+        global trials_to_trigger, trials_so_far
+        if trials_to_trigger == -1:
+            trials_to_trigger = trials_so_far
+            self.trials_results[exp_name] = trials_to_trigger
+
+    def save_results(self, val: float, exp_name: str):
+        # Infinity
+        if math.isinf(val):
+            if exp_name not in self.results.keys():
+                if val < 0.0:
+                    self.results[exp_name] = [1, 0, 0, 0, 0]
+                    self.save_trials_to_trigger(exp_name)
+                else:
+                    self.results[exp_name] = [0, 1, 0, 0, 0]
+                    self.save_trials_to_trigger(exp_name)
+            else:
+                if val < 0.0:
+                    self.results[exp_name][0] += 1
+                else:
+                    self.results[exp_name][1] += 1
+
+        # Subnormals
+        if numpy.isfinite(val):
+            if val > -2.22e-308 and val < 2.22e-308:
+                if val != 0.0 and val != -0.0:
+                    if exp_name not in self.results.keys():
+                        if val < 0.0:
+                            self.results[exp_name] = [0, 0, 1, 0, 0]
+                            self.save_trials_to_trigger(exp_name)
+                        else:
+                            self.results[exp_name] = [0, 0, 0, 1, 0]
+                            self.save_trials_to_trigger(exp_name)
+                    else:
+                        if val < 0.0:
+                            self.results[exp_name][2] += 1
+                        else:
+                            self.results[exp_name][3] += 1
+
+        if math.isnan(val):
+            if exp_name not in self.results.keys():
+                self.results[exp_name] = [0, 0, 0, 0, 1]
+                self.save_trials_to_trigger(exp_name)
+            else:
+                self.results[exp_name][4] += 1
+
+    def update_runs_table(self, exp_name: str):
+        if exp_name not in self.runs_results.keys():
+            self.runs_results[exp_name] = 0
+        else:
+            self.runs_results[exp_name] += 1
+
+    def are_we_done(self, func, recent_val, exp_name):
+        global found_inf_pos, found_inf_neg, found_under_pos, found_under_neg
+
+        # Finding INF+
+        if 'max_inf' in func.__name__:
+            if found_inf_pos:
+                return True
+            else:
+                if is_inf_pos(recent_val):
+                    found_inf_pos = True
+                    self.save_results(recent_val, exp_name)
+                    return True
+
+        # Finding INF-
+        elif 'min_inf' in func.__name__:
+            if found_inf_neg:
+                return True
+            else:
+                if is_inf_neg(recent_val):
+                    found_inf_neg = True
+                    self.save_results(recent_val, exp_name)
+                    return True
+
+        # Finding Under-
+        elif 'max_under' in func.__name__:
+            if found_under_neg:
+                return True
+            else:
+                if is_under_neg(recent_val):
+                    found_under_neg = True
+                    self.save_results(recent_val, exp_name)
+                    return True
+
+        # Finding Under+
+        elif 'min_under' in func.__name__:
+            if found_under_pos:
+                return True
+            else:
+                if is_under_pos(recent_val):
+                    found_under_pos = True
+                    self.save_results(recent_val, exp_name)
+                    return True
+        return False
+
+    def get_results(self):
+        return self.results
+
+    def get_random_results(self):
+        return self.random_results
+
+    def print_result(self, shared_lib, number_sampling, range_splitting, logger):
+        function_key = shared_lib + '|' + number_sampling + '|b_' + range_splitting
+        fun_name = os.path.basename(shared_lib)
+        print('-------------- Results --------------')
+        print(fun_name)
+        if len(self.results.keys()) > 0:
+            total_exception = 0
+            for key in self.results.keys():
+                if function_key in key:
+                    print('Range:', key.split('|')[0])
+                    print('\tINF+:', self.results[key][0])
+                    print('\tINF-:', self.results[key][1])
+                    print('\tSUB-:', self.results[key][2])
+                    print('\tSUB-:', self.results[key][3])
+                    print('\tNaN :', self.results[key][4])
+                    # print('\tTotal Exception for range {}: {}'.format(key.split('|')[0], sum(results[key]))
+                    total_exception += sum(self.results[key])
+            print('\tTotal Exception: ', total_exception)
+            logger.info('\tTotal Exception: {} '.format(total_exception))
+        else:
+            print('\tINF+:', 0)
+            print('\tINF-:', 0)
+            print('\tSUB-:', 0)
+            print('\tSUB-:', 0)
+            print('\tNaN :', 0)
+        print('')
