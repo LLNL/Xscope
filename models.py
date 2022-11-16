c00ff311f860d141a91eac2d1848d46ec5ef1e3f
Anh Tran <anhb.tran1010@gmail.com>
2022-11-16 10:33:58 -0700

    some refactoring

---
+++ models.py
@@ -0,0 +1,27 @@
+import gpytorch
+
+class ExactGPModel(gpytorch.models.ExactGP):
+    """
+    Class for Gaussian Process Model
+    Attributes
+    ----------
+    train_x: a Torch tensor with shape n x d
+        The training data
+    train_x: a Torch tensor with shape n x 1
+        The label
+    likelihood : The likelihood function
+        A function that approximate the likelihood of the GP given new observed datapoint
+    Methods
+    -------
+    forward
+    """
+
+    def __init__(self, train_x, train_y, likelihood):
+        super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
+        self.mean_module = gpytorch.means.ConstantMean()
+        self.covar_module = gpytorch.kernels.MaternKernel()
+
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
