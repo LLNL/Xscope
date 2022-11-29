6a1dacc178c2fa864ee907e7b261648293ad30c7
Ignacio Laguna <ilaguna@llnl.gov>
2022-11-29 09:10:36 -0800

    new experiments in Lassen

--- experiments/submit.sh
+++ experiments/submit.sh
@@ -1,7 +1,22 @@
 #!/bin/bash -x
 
 bsub script_1.cmd
-#bsub script_2.cmd
+bsub script_2.cmd
 bsub script_3.cmd
-#bsub script_4.cmd
-#bsub script_5.cmd
+bsub script_4.cmd
+bsub script_5.cmd
+bsub script_6.cmd
+bsub script_7.cmd
+bsub script_8.cmd
+bsub script_9.cmd
+bsub script_10.cmd
+bsub script_11.cmd
+bsub script_12.cmd
+bsub script_13.cmd
+bsub script_14.cmd
+bsub script_15.cmd
+bsub script_16.cmd
+bsub script_17.cmd
+bsub script_18.cmd
+bsub script_19.cmd
+bsub script_20.cmd
